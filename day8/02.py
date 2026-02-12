9c7004cfd994d56b23306d17fba80dba1a59e636
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2026-02-12 12:36:45 +0530

    Implement pie chart visualization in 02.py
    Add pie chart plotting functionality using matplotlib

---
+++ day8/02.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# plt.plot([1,2,3,4,5], [11,55,33,22,66])
+
+# plt.xlabel("month")
+# plt.ylabel("sales")
+
+# plt.title("Sales by Month")
+
+# plt.show()
+
+plt.pie([10,22,14,50,29])
+
+plt.show()
